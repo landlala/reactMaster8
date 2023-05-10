ef617ebc2c4940639f1594c29a681114128389e6
landlala <land_lala@naver.com>
2023-05-10 06:23:38 +0000

    #8.9

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,24 +1,13 @@
 import {useRef, useEffect} from "react";
 import styled from "styled-components";
-import {motion, useMotionValue, useTransform, useMotionValueEvent} from "framer-motion";
+import {motion, useMotionValue, useTransform, useMotionValueEvent, useScroll} from "framer-motion";
 
 const Wrapper = styled(motion.div)`
-  height: 100vh;
+  height: 200vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
-`;
-
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.4);
-  border-radius: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
 `;
 
 const Box = styled(motion.div)`
@@ -46,12 +35,21 @@ const boxVariants = {
 
 function App() {
   const x = useMotionValue(0);
-  const scale = useTransform(x, [-800, 0, 800], [2, 1, 0.1]);
-  useMotionValueEvent(scale, "change", (l) => console.log(l));
+  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+  const gradient = useTransform(
+    x, 
+    [-800, 800], 
+    [
+      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
+    ]
+  );
+  const {scrollYProgress} = useScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
   return (
-    <Wrapper>
+    <Wrapper style = {{background: gradient}}>
       <Box
-        style = {{x, scale}}
+        style = {{x, rotateZ, scale}}
         drag = "x" 
         dragSnapToOrigin
       />
