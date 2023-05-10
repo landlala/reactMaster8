27b4e6d79ce9cdf55abf94cd7fb3c1d4c097ac3c
landlala <land_lala@naver.com>
2023-05-10 02:15:59 +0000

    #8.1

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -13,7 +13,7 @@ const Box = styled.div`
   width: 200px;
   height: 200px;
   background-color: white;
-  border-radius: 10px;
+  border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
